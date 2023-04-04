8636c4b2007b1d27b853758b83271410a8308050
Pxe2k <roariffic474@gmail.com>
2023-04-04 12:07:20 +0600

    feat: mock products file

---
+++ src/mock/products.js
@@ -0,0 +1,42 @@
+export const products = [
+  {
+    imgSrc: 'https://cdn.food.ru/unsigned/fit/640/480/ce/0/czM6Ly9tZWRpYS9waWN0dXJlcy9yZWNpcGVzLzE2MzM1MS9jb3ZlcnMvaFlzdHlVLmpwZWc.webp',
+    name: 'Гренки из батона с чесноком на сковороде',
+    price: 1800
+  },
+  {
+    imgSrc: 'https://cdn.food.ru/unsigned/fit/640/480/ce/0/czM6Ly9tZWRpYS9waWN0dXJlcy9yZWNpcGVzLzE2MzQwNi9jb3ZlcnMvM3VERURLLmpwZWc.webp',
+    name: 'Ростбиф из говядины классический',
+    price: 2600
+  },
+  {
+    imgSrc: 'https://cdn.food.ru/unsigned/fit/640/480/ce/0/czM6Ly9tZWRpYS9waWN0dXJlcy9yZWNpcGVzLzE2MzQyNi9jb3ZlcnMvNFZiVzNaLmpwZWc.webp',
+    name: 'Салат с яблоками, сыром и яйцом',
+    price: 1900
+  },
+  {
+    imgSrc: 'https://cdn.food.ru/unsigned/fit/640/480/ce/0/czM6Ly9tZWRpYS9waWN0dXJlcy9yZWNpcGVzLzE2MzUzOS9jb3ZlcnMvM2hhWmhyLmpwZWc.webp',
+    name: 'Сливочная паста с индейкой, грибами, сыром',
+    price: 2400
+  },
+  {
+    imgSrc: 'https://cdn.food.ru/unsigned/fit/640/480/ce/0/czM6Ly9tZWRpYS9waWN0dXJlcy9yZWNpcGVzLzE2MzY0Mi9jb3ZlcnMvWVdBNjVTLmpwZWc.webp',
+    name: 'Салат крабовый классический с кукурузой и рисом',
+    price: 2100
+  },
+  {
+    imgSrc: 'https://cdn.food.ru/unsigned/fit/640/480/ce/0/czM6Ly9tZWRpYS9waWN0dXJlcy9yZWNpcGVzLzE2MzM1MC9jb3ZlcnMvNDdyOG9iLmpwZWc.webp',
+    name: 'Тарталетки с грибами и сыром',
+    price: 2200
+  },
+  {
+    imgSrc: 'https://cdn.food.ru/unsigned/fit/640/480/ce/0/czM6Ly9tZWRpYS9waWN0dXJlcy9yZWNpcGVzLzE2MzUzOC9jb3ZlcnMvcW9mdTJCLmpwZWc.webp',
+    name: 'Котлеты из фарша в панировочных сухарях',
+    price: 2300
+  },
+  {
+    imgSrc: 'https://cdn.food.ru/unsigned/fit/640/480/ce/0/czM6Ly9tZWRpYS9waWN0dXJlcy9yZWNpcGVzLzE2MzQwNS9jb3ZlcnMveXhSaXpiLmpwZWc.webp',
+    name: 'Кабачковые оладьи на сковороде',
+    price: 1900
+  },
+]
